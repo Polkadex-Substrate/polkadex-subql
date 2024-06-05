7e2860d514c9bf6b64168b7295e9aab284a02a87
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-06-05 15:44:52 +0530

    fix: new asset id parser

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,9 @@
 import {SubstrateEvent,} from "@subql/types";
 import {Account, TheaDeposit, TheaWithdrawal} from "../types";
 import {AccountId32, Balance} from "@polkadot/types/interfaces";
-import {u128, u32, u8, Vec} from "@polkadot/types-codec"
+import {u32, u8, Vec} from "@polkadot/types-codec"
 import {Status} from "./types";
+import type {Codec} from "@polkadot/types-codec/types";
 
 const TheaEvents = {
     /// Deposit Approved event ( Network, recipient, asset_id, amount, id))
@@ -42,7 +43,7 @@ export async function handleTheaEvents(event: SubstrateEvent): Promise<void> {
             block.block.hash.toString(),
             (chain_id as u8).toNumber(),
             (amount as Balance).toBigInt(),
-            (asset_id as u128).toBigInt(),
+            BigInt(assetIdToNumeric(asset_id)),
             (recipient as AccountId32).toString(),
             (recipient as AccountId32).toString(),
             Status.APPROVED,
@@ -71,7 +72,7 @@ export async function handleTheaEvents(event: SubstrateEvent): Promise<void> {
             block.block.hash.toString(),
             (network_id as u32).toNumber(),
             (amount as Balance).toBigInt(),
-            (asset_id as u128).toBigInt(),
+            BigInt(assetIdToNumeric(asset_id)),
             beneficiary.toString(),
             user.toString(),
             Status.QUEUED,
@@ -96,3 +97,19 @@ export async function handleTheaEvents(event: SubstrateEvent): Promise<void> {
         logger.info("UNKNOWN EVENT: THEAHANDLER: ", JSON.stringify(event))
     }
 }
+
+function assetIdToNumeric(asset_id: Codec): string {
+    let asset = (asset_id.toJSON() as Record<string, string | null>)
+    //check if asset is u128
+    if (Object.keys(asset).length === 0) {
+        return asset_id.toJSON() as string
+    }
+    //get first key of asset
+    let key = Object.keys(asset)[0]
+    //check if asset is polkadex then value is 0
+    if (key.toLowerCase() === "polkadex" || key.toLowerCase() === "pdex") {
+        return '0'
+    }
+    return asset[key] as string
+}
+[1,"esq2wFkRsic8WM4nstAtkjqWdCDnTrGHMpFjaGN2rEHnQXUNm","0x03010200a10f0100bccb86c5fe9bb883ad03d0456646292a7ad187ac2ab38204134c112b4a99747d",{"asset":"0x02a1763880a2113075cecf0355615dc6"},100000000000,"0x0000000081e034417703e8e8d946c8e847c1456a"]
